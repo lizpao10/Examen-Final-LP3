83d6ab5afc19a641554b28a3ebdfe68a554e803b
carinme <carinme97@fpuna.edu.py>
2022-06-16 22:04:21 -0400

    Fix Tema2

--- Tema2.c
+++ Tema2.c
@@ -1,89 +1,118 @@
-#include <stdio.h>
-#include <unistd.h>
+
+   #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
 #include <pthread.h>
 
 #define MAX 20
-int varGlob = 0;
 
-struct estructura{
-    int vec[MAX];
-    int value;
-    int hilo;
-    int flag;
-    int n;
-};
+typedef struct s_indices
+{
+	int ini;
+	int fin;
+}indices;
 
-void *thread_secuencial(void *arg){
-    struct estructura *vector = (struct estructura *)arg;
+void put_numbers();
+int rand_limit(int lim);
+void buscar(void *arg);
 
-    if(varGlob==0){
-        for(int r=0; r<vector->n; r++){
-            if(vector->value == vector->vec[r]){
-                int hilo = vector->hilo-1;
-                printf("Se encontro el valor %d en el hilo # %d en la posicion %d", vector->value, vector->hilo, (vector->n*hilo)+r);
-                varGlob = 1;
-                break;
-            }
-        }
-    }
+int num_objetivo = -1;
+int terminacion = 0;
+int *array;
+
+int main(int argc, char **argv)
+{
+	int i, cant_hilos = atoi(argv[2]);
+	num_objetivo = atoi(argv[1]);
+	
+	array = malloc(MAX*sizeof(int));
+	
+	srandom(time(0));
+	put_numbers(array);
+	
+	if(cant_hilos > MAX)
+		cant_hilos = MAX;
+	pthread_t hilo[cant_hilos];
+	
+	int tam2 = MAX/cant_hilos;
+   	
+   	indices index[cant_hilos];
+   	for(i = 0; i < cant_hilos; i++)
+   	{
+   		index[i].ini = i*tam2;
+   		if(i == cant_hilos-1)
+   			index[i].fin = MAX;
+   		else
+   			index[i].fin = i*tam2 + tam2;
+   		pthread_create(&hilo[i], NULL, (void*)&buscar, (void*)&index[i]);
+   	}
+   	
+   	void *flag;
+   	int aux;
+   	indices result;
+   	result.ini = -1; result.fin = -1;
+   	for(i = 0; i < cant_hilos; i++)
+   	{
+   		pthread_join(hilo[i], &flag);
+   		aux = (int)flag;
+   		if(aux >= 0)
+   		{
+   			//printf("Flag: %d\n", aux);
+   			result.ini = aux;
+   			result.fin = i;
+   		}
+   	}
+   	
+   	printf("Array:\n");
+   	for(i = 0; i < MAX; i++)
+   		printf("%d ", array[i]);
+   	printf("\n\n"); 
+   	
+   	if(result.ini >= 0) 
+   		printf("El numero %d fue encontrado en el indice %d por el proceso %d.\n", num_objetivo, result.ini, result.fin);
+   	else
+   		printf("El numero %d no fue encontrado.\n", num_objetivo);
+	
+	return 0;
 }
 
+void buscar(void *arg)
+{
+	indices index =  *((indices*)arg);
+	int i;
+	for(i = index.ini; i < index.fin && terminacion == 0; i++)
+	{
+		if(array[i] == num_objetivo)
+		{
+			terminacion = 1;
+			//printf("Se encontro\n");
+			int aux1 = (int) i; //Ver el comentario de abajo
+			return (void*) i;
+		}
+	}
+	//printf("No se encontro\n");
+	int menos_uno = -1;
+	int aux2 = (int) menos_uno; //No se porque, pero esta linea "innecesaria" hace que retorne corretamente el numero
+	return (void*) menos_uno;
+}
 
-int main(int argc, char *argv[]){ 
-    time_t t;
-    int i, v[MAX],value=0,cantidades=0, j=0, hilos=0, n=0,m=0, k=0, numero, flag=0;
-    srandom(time(&t)); 
-    for (i=0;i<MAX;i++){
-        numero=random()%10;
-        v[i]=numero;
+int rand_limit(int lim)
+{
+	int divisor = RAND_MAX/lim;
+	int valor;
+	
+	do{
+	valor = random()/divisor;
+	}while(valor > lim);
+	
+	return valor;
+}
 
-    }
-    for(i=0; i<MAX; i++){
-        printf("%d ",v[i]);
-    }
-    printf("\n");
-
-
-
-    if (argv[0])value=atoi(argv[1]);
-    if (argv[1])cantidades=atoi(argv[2]);
-    n=MAX/cantidades;
-    m=n;
-    int vector[n];
-    for(int r=0;r<n;r++)vector[r]=0;
-    struct estructura vec_struct[cantidades];
-    
-
-    
-    pthread_t thread[cantidades];      
-    for (int i=0; i<cantidades; i++){
-        for (int r=j; r<m; r++){
-            vector[k] = v[r];
-            vec_struct[i].vec[k] = v[r];
-            vec_struct[i].hilo = i+1;
-            vec_struct[i].value = value;
-            vec_struct[i].flag = 0;
-            vec_struct[i].n = n;
-            k++;
-        }
-
-        k=0;
-        j=m;
-        m=m+n;
-
-    }
-    for (int i=0; i<cantidades; i++){
-        if(varGlob==0)pthread_create(&thread[i],NULL,&thread_secuencial,&vec_struct[i]);
-        else break;
-    }
-
-    for (int i=0; i<cantidades; i++)pthread_join(thread[i],NULL);
-
-
-
-
-    printf("\n");
-    return 0;
+void put_numbers()
+{
+	int i;
+	for(i = 0; i < MAX; i++)
+	{
+		array[i] = rand_limit(10);
+	}
 }
