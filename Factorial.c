8cbd54780f80fc0bff65d6a7b720494ef8ed1e45
Paola <pao081996@fpuna.edu.py>
2022-06-16 21:46:07 -0400

    Codigo casi true

--- Factorial.c
+++ Factorial.c
@@ -1,36 +1,57 @@
 
-#include <stdio.h>
-#include <stdlib.h>
-#include <pthread.h>
-
-struct param_t {
-    int num;
-    int result;
-};
-
-int factorial(int n)
+#include<stdio.h>
+#include<stdlib.h>
+#include<pthread.h>
+unsigned long long *factorial(unsigned long long n)
 {
-    if (n >= 1)
-        return n * factorial(n - 1);
-    else
-        return 1;
+    unsigned long long *r= (unsigned long long*)malloc(1*sizeof(unsigned long long));
+    unsigned long long i= 1,f= 1;
+    while (i<= n)
+    {
+        f= f*i;
+        i= i +1;
+    }
+    r[0]=f;
+    printf("Factorial de %lld: %lld\n", n, f);
+    return r;
 }
 
-void *thread_func(void *args)
+void *factorial_(void *args)
 {
-    struct param_t *param = (struct param_t *)args;
-    param->result = factorial(param->num);
-    return (void *)NULL;
+    unsigned long long n= *((unsigned long long*)args);
+    return (void*)factorial(n);
 }
 
-int main()
-{
-    struct param_t param;
-    pthread_t thread;
-    printf("Introduzca los números para encontrar su factorial: ");
-    scanf("%d", &param.num);
-    pthread_create(&thread, NULL, thread_func, &param);
-    pthread_join(thread, NULL);
-    printf("Factorial de Number: %d/n", param.result);
-    return 0;
+int main(int argc , char **argv){
+    int conta=0;
+    char lectura[10000];
+    printf("Ingresar valores para calcular factorial:/n ");
+	scanf("%[^\n]%*c",lectura);
+	char *p;
+	p =lectura;
+	while (*p != '\0') {
+		if( *p == ' ') conta++;
+		p++;
+	}
+    unsigned long long valores[conta+1];
+    int j=0;
+    char *a;
+    a =lectura;
+    while (*a != '\0') {
+		if( !(*a == ' '))
+        {
+            valores[j]= (unsigned long long) strtol(a, NULL, 10);
+            if(valores[j]>9)a++;
+            j++;
+        } 
+		a++;
+	}
+    pthread_t thread[conta+1];
+    unsigned long long auxi;
+    for (int i = 0; i < conta+1; i++)
+    {
+        auxi= valores[i];
+        pthread_create(&thread[i], NULL, factorial_,&auxi);
+        pthread_join(thread[i],NULL);
+    } 
 }
